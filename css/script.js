20d994679772e3943b9dfc0f43bd6effa807a99e
Irfanbd <gourob83@gmail.com>
2021-11-29 20:00:07 +0600

    Update script.js

---
+++ css/script.js
@@ -0,0 +1,20 @@
+let weather = {
+  apikey: "6d9918101c873bfdbc8c889cb3ff0834",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&APPID=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+  },
+};
